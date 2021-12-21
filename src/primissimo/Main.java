ef1be9eb7770047b31bf84f59c4f87e83bedf306
Astaroth <Astaroth@DESKTOP-PNGQC7F.lan>
2021-12-21 11:51:48 +0100

    commit1

---
+++ src/primissimo/Main.java
@@ -0,0 +1,17 @@
+package primissimo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		int i;
+		int j;
+		int y = 5;
+		for (i = 0; i < 5; i++) {
+			for (j = 0; j < y; j++) {
+				y--;
+			}
+		}
+	}
+
+}
